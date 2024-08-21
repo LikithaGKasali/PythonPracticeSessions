70b2874915bf08dfd3ce5ebb06f62f8df15e3143
Likku <likitha.g#metricstream.com>
2024-08-21 11:11:14 +0530

    d

--- PythonPractice/PythonCOdes/min_ele.py
+++ PythonPractice/PythonCOdes/min_ele.py
@@ -1,8 +1,10 @@
-input_list = [11, 1, 2, 3, 4, 10,0, 5, 6, 7, 8, 9]
+import infinity as infinity
+
+input_list = [11, 3, 4, 10, 5, 6, 7, 8, 9]
 # print(max(input_list))
 # print(len(input_list))
 # print(min(input_list))
-Ans = 10000
+Ans = float('inf')
 for i in input_list:
     if i < Ans:
         Ans = i
